b0aae98762662633811e8d73c729616195e68a85
Allan Miller <millerallan17@gmail.com>
2024-07-30 08:44:23 -0300

    apagueos arquivos no fireBse

--- lib/services/notifications/firebase_credentials.dart
+++ lib/services/notifications/firebase_credentials.dart
@@ -1,13 +1,1 @@
-const Map<String, dynamic> firebaseCredentials = {
-  "type": "service_account",
-  "project_id": "spaceimoveis-72b28",
-  "private_key_id": "e4e6071f0e92ca24d55b60be1c2a85d18b758b85",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDbDh0qa2WMNQMd\nOh4iFY4LHIc0MEW9pavZR60XrYJTI3esQvlTBgU/plt92DTojh7QXM/bRX6xgIO7\nXbVYPXeQgnzERaPXKv0/ppZm78aw5jf32wHsXz3hNh0tT8VgMOwq9STnNpNGK06g\nQvvwfVmMfwL8vZek4eSDpXLv3nki4ykgdQMbesbMvYV0z5Ctd7SDOAd+bdWw4vnq\nTHj0C5XMWQVSR+Lh7YZZAuBc9pmaFgpDUStdsHT2w6B3E22kTvL4cnPhqc8S9lpO\nqIV0hicc8yxb6HPAq+fwuOp1NDh14ynkN5V3s2tG5toEuVS8n+YmE0ZGF03G+/Od\nHlxrQskXAgMBAAECggEAHvVeMDC7H8xy3bkT1CZZYP74VBW0bYqSAHnC9UiLgCYl\nk34S+uWsF+RHeZV1A3Q+gkfJBTN2UINvWqtyOyp+EOQthOxWAk1W5Z2CE8HfF1Yv\nYm3x1iRZvE/9DKUsVHK66zv31LOKWeLkpagB2j76SRhpkCsCrltR2Tx97iJHhXie\njy/8JbOloIDMFtPnFJlj9u9cyHK9y+hwVI2/36XpPcnaZvFHAftj0Htt4n97MwII\nAz/qvm/rkFyFStqKPU1U77Ws8XUTNB+dGf4ndfKH+BkLJlkLmpRA43f1EW43t7Sv\nmqKt6IPBsyRaEeuPaZIOesABpGk4GvqbTyJxku5noQKBgQD7LWFIJXMnLfD3ouEr\n3QI0hKwfwRspBN3rB8R3cUKlV1I0ktQ4SvuHSlnBv+CX9wAgyO6pvLw3MVcMU4rx\nCBxWinOHq0gIJfrxxyAZ7G1ZDucq7eLnYaP5hQrMEfV1ReNFkblurmnQTPgZ7QI+\nCDSqKnFPFYJ9QN8tuCoeT96SNwKBgQDfQtfKMJSFCnvi1cvdmRuBLH8+uJrr6UQ/\ntb+uxoCqX0M9KgT8b7IYYOBS1xKF4wu7INzrzMyf+aHp6DbvMqs2l2kEQUV18jvm\nASABLDb9RekEPiZLBOq7NUILGAtkA3wI1+QHYfU9WKaxgo5Vo/cS9lqc8MG5L6tO\nPwvyX8WQIQKBgFYOqEQFwLxtux+9SJS0tiq0QhznFp0pM7dxA9lAjusDTgEBRYC8\nAA3MZZBCY3rtf7e74Dl7lqj6WK66eOI4Oy1TZl3XS2ivSplH9jGrlZNrzbwhoRpR\nk9LLyHU+ufCiL0oIsZXAYDSkCmUnlRsXyKN8wzGrVvf/H/gUHyofh0xlAoGBAItZ\nRCi9w6V6UrLVMKaCbslkRZOXU5LViLSfMyHaYep1BRnDBVOuP6G5ranm9Xe9q8/D\nhkBjFoDvJaFCQC9yF60+OdzfBITGiZ5Yq2ov966DmRn91JbUXdI1PqCABHQLPLqk\nTX5SJOiCUaDP1Rkc+xTJ0OrN1oxIdYqkDUfdgQkhAoGBAJsCKUFaSKZtl+c7LcH1\niIabOoZKpFEDzpS5aJhjoi2Uhrfd2oEfBp2/yDtGSDdOpPYAIeRnymDQNGMm+Rjw\nbUyt9C8Kd124FyyWUtSSiTjMG7C/ofWYMcajH9jTsDrXHAqiw2F27cknq+hW47GK\nGaTq35RKX0mqF4eRKELrwuls\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-j15ct@spaceimoveis-72b28.iam.gserviceaccount.com",
-  "client_id": "117138093227442188933",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-j15ct%40spaceimoveis-72b28.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-};
+
